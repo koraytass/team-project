493faf1db260a4237d6033bd15808716a4119d43
YiitWT <yigit.best@outlook.com>
2025-05-09 16:59:11 +0300

    🥶🥶  Suppa duppa guessing game

---
+++ GuessingGame.cs
@@ -0,0 +1,78 @@
+using System;
+using System.Diagnostics.CodeAnalysis;
+using System.Drawing;
+using System.Runtime.InteropServices;
+
+
+public class GuessingGame
+{
+    public int maxTries = 3;
+    public int maxRange = 9;
+    public void Play()
+    {
+        Random rn = new Random();
+        int answer = rn.Next(0,maxRange+1);
+        int? prevDiff = null;
+        for (int i = 0; i < maxTries + 1; i++)
+        {
+            if (i == maxTries)
+            {
+                Console.BackgroundColor = ConsoleColor.White;
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Womp womp u couldnt guess it");
+                Console.BackgroundColor = ConsoleColor.Black;
+                break;
+            }
+            Console.WriteLine($"You have {maxTries - i} tries left.");
+            Console.Write("Pick a number between 0-"+maxRange+" : ");
+            int guess = Convert.ToInt32(Console.ReadLine());
+            if (guess == answer)
+            {
+                Console.BackgroundColor = ConsoleColor.Green;
+                Console.WriteLine("🥳🥳🥳 Woahh you guessed it.");
+                Console.BackgroundColor = ConsoleColor.Black;
+                break;
+            }
+
+            int currDiff = Math.Abs(answer - guess);
+
+            if (prevDiff.HasValue)
+            {
+                if (currDiff < prevDiff)
+                {
+                    Console.BackgroundColor = ConsoleColor.Red;
+                    Console.WriteLine("🔥 Getting hotter!");
+                    Console.BackgroundColor = ConsoleColor.Black;
+                }
+                else if (currDiff > prevDiff)
+                {
+                    Console.BackgroundColor = ConsoleColor.Blue;
+                    Console.WriteLine("🥶 Getting colder!");
+                    Console.BackgroundColor = ConsoleColor.Black;
+                }
+                else
+                {
+                    Console.BackgroundColor = ConsoleColor.Yellow;
+                    Console.WriteLine("😐 Neither hotter or colder.");
+                    Console.BackgroundColor = ConsoleColor.Black;
+                }
+            }
+
+            prevDiff = currDiff;
+        }
+
+
+    }
+}
+
+public class Program
+{
+    public static void Main()
+    {
+        GuessingGame game = new GuessingGame();
+        game.maxRange = 9;
+        game.maxTries = 3;
+        game.Play();
+
+    }
+}
--- calc.cs
+++ calc.cs
@@ -2,66 +2,80 @@ using System;
 
 namespace HelloWorld
 {
-  
+
   public class Calculator
   {
-    public double sum(double x, double y){
-      return x+y;
+    public double sum(double x, double y)
+    {
+      return x + y;
     }
     public double substract(double x, double y)
     {
-      return x-y;  
+      return x - y;
     }
-    public double divide(double x, double y){
-      if(x == 0 || y == 0) {
-		Console.WriteLine("Cannot be divided 0");
+    public double divide(double x, double y)
+    {
+      if (x == 0 || y == 0)
+      {
+        Console.WriteLine("Cannot be divided 0");
         return 0;
       }
-      return (x/y);
+      return (x / y);
     }
-    public double multiply(double x, double y){
+    public double multiply(double x, double y)
+    {
       return x * y;
     }
   }
-  
-  
-  
-	public class Program
-	{
-		public static void Main(string[] args)
-		{
-            double sonuc = 0;
-            do
-            {
-          Console.Write("1. Sayı: ");
-		  double x = Convert.ToDouble(Console.ReadLine());
-          Console.Write("\n2. Sayı: ");
-		  double y = Convert.ToDouble(Console.ReadLine());
-		  Calculator calc = new Calculator();
-		  Console.WriteLine("Choose operation (+,-,*,/)");
-          string operation = Console.ReadLine();
-          switch (operation)
-          {
-			 case "+":
-             sonuc = calc.sum(x,y);
-			  break;
-            case "-":
-             sonuc = calc.substract(x,y);
-                break;
-            case "*":
-             sonuc = calc.multiply(x,y);
-                break;
-            case "/":
-             sonuc = calc.divide(x,y);
-                break;
 
-            default:
-                System.Console.WriteLine("Womp womp idk what u sayin");
-                break;
-          }
-          System.Console.WriteLine($"Sonuç : {sonuc}");
-		}while(sonuc!=0);
-        System.Console.WriteLine("Ended cuz the result is 0.");
+
+
+  public class Program
+  {
+    public static void Main(string[] args)
+    {
+      double sonuc = 0;
+      string _operation;
+      do
+      {
+        Console.Write("1. Sayı: ");
+
+        double x = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("\n2. Sayı: ");
+
+        double y = Convert.ToDouble(Console.ReadLine());
+
+        Calculator calc = new Calculator();
+
+        Console.WriteLine("Choose operation (+,-,*,/)");
+        
+        string operation = Console.ReadLine();
+
+        _operation = operation;
+        
+        switch (operation)
+        {
+          case "+":
+            sonuc = calc.sum(x, y);
+            break;
+          case "-":
+            sonuc = calc.substract(x, y);
+            break;
+          case "*":
+            sonuc = calc.multiply(x, y);
+            break;
+          case "/":
+            sonuc = calc.divide(x, y);
+            break;
+
+          default:
+            System.Console.WriteLine("Womp womp idk what u sayin");
+            break;
         }
-	}
-} 	
+        System.Console.WriteLine($"Sonuç : {sonuc}");
+      } while (_operation == "-" && sonuc != 0);
+      System.Console.WriteLine("Ended cuz the result is 0.");
+    }
+  }
+}

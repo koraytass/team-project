03d5fde1c2190d94eb4fd1ee53c20c676466324c
Ecrin <ecrin_m@icloud.com>
2025-05-13 12:49:56 +0300

    bu benim oyunum

---
+++ oyun/script.js
@@ -0,0 +1,23 @@
+const randomNumber = Math.floor(Math.random() * 10) + 1;
+
+document.getElementById("guessButton").addEventListener("click", function () {
+    const userGuess = Number(document.getElementById("guessInput").value);
+    const resultText = document.getElementById("resultText");
+
+    if(!userGuess || userGuess < 1 || userGuess > 10) {
+        resultText.textContent = "pleas enter a number between 1 and 10.";
+        resultText.style.color = "red";
+        return;
+    }
+
+ if (userGuess === randomNumber) {
+    resultText.textContent = "🎉 congratulation! You guessed it!";
+    resultText.style.color = "green";
+ } else if (userGuess < randomNumber) {
+    resultText.textContent = "Try a higher number!";
+    resultText.style.color = "#333";
+ } else {
+    resultText.textContent = "Try a lower number!";
+    resultText.style.color = "#333";
+ }
+});   
